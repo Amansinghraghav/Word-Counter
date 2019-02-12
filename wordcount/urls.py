7d8c94613583458ffb830e3b0eae31e135129ff8
Amansinghraghavcd <amanraghav109@gmail.com>
2019-02-12 15:23:08 +0530

    third version

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -19,5 +19,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.homepage, name='home'),
-    path("countsss",views.count,name='count')
+    path("count",views.count,name='count')
 ]
